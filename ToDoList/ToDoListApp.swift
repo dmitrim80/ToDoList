7b6faa0dde0a16816c0e0e1d5e497bb4485011e5
Dmitri Morozov <dmitri.m80@gmail.com>
2023-05-26 15:33:35 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Dmitri Morozov on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
