13365cbee21295e6f00cb67499ee2e7c0542aacc
Dmitri Morozov <114444260+dmitrim80@users.noreply.github.com>
2023-06-05 07:19:32 -0400

    Added saveItems function, encoded Data and decoded

--- ToDoList/Models/ItemModel.swift
+++ ToDoList/Models/ItemModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // immutable struct
-struct ItemModel: Identifiable {
+struct ItemModel: Identifiable, Codable {
     let id: String
     let title: String
     let isCompleted: Bool
--- ToDoList/ViewModels/ListViewModel.swift
+++ ToDoList/ViewModels/ListViewModel.swift
@@ -9,18 +9,29 @@ import Foundation
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItems()
+        }
+    }
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first title!", isCompleted: false),
-            ItemModel(title: "This is the second!", isCompleted: true),
-            ItemModel(title: "Third!", isCompleted: false),]
-        items.append(contentsOf: newItems)
+//        let newItems = [
+//            ItemModel(title: "This is the first title!", isCompleted: false),
+//            ItemModel(title: "This is the second!", isCompleted: true),
+//            ItemModel(title: "Third!", isCompleted: false),]
+//        items.append(contentsOf: newItems)
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else {return}
+        
+        self.items = savedItems
     }
     func deleteItem(indexSet: IndexSet) {
         items.remove(atOffsets: indexSet)
@@ -43,4 +54,10 @@ class ListViewModel: ObservableObject {
             items[index] = item.updateCompletion()
         }
     }
+    func saveItems() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
+        }
+    }
+    
 }
